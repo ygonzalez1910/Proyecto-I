e1d437ee166f3392e3546b8135382fc15b759508
Yuli Gonzalez <ygonzalez1910@gmail.com>
2023-09-12 08:17:24 -0600

    implementacionTipoInstIncompleta

--- Proyecto I - Programacion III/src/HELLO.java
+++ Proyecto I - Programacion III/src/HELLO.java
@@ -15,6 +15,16 @@ public class HELLO extends JFrame{
     private JButton reporteButton;
     private JButton buscarButton;
     private JTable Listado;
+    private JTextField txtCodTipoInstrumento;
+    private JTextField txtNomTipoIns;
+    private JTextField txtUnidadTipIns;
+    private JButton guardarTI;
+    private JButton limpiarTI;
+    private JButton borrarTI;
+    private JTextField txtBuscarNombreTI;
+    private JButton reporteTIButton;
+    private JButton buscarTIButton;
+    private JTable ListadoTipInst;
 
 
     public HELLO(){
